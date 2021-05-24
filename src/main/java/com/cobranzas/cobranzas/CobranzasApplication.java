218912d316c26c46353d631ee096df2125154698
Santiago Hernan Duque Tamayo - Ceiba Software <santiago.duque@ceiba.com.co>
2021-05-24 15:57:54 -0500

    Estructura de proyecto spring creado con gradle

---
+++ src/main/java/com/cobranzas/cobranzas/CobranzasApplication.java
@@ -0,0 +1,13 @@
+package com.cobranzas.cobranzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobranzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobranzasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cobranzas/cobranzas/CobranzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobranzas.cobranzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobranzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
